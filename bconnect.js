201c2ea1e738853517e4d73b115dd452413187f0
Kanaaan <mapjs@mapjs.com>
2016-02-20 22:19:27 +0100

    * Load module  and use exported function

--- bconnect.js
+++ bconnect.js
@@ -1,31 +1,34 @@
-const net = require('net');
-const fs = require('fs');
 
-var client = new net.Socket();
-var count=0;
-var filename='1090Logger_'+(new Date().toISOString().replace(/:/,'_').replace(/:/,'_'))+'.txt';
-console.log(filename);
+exports.connectToADSBServer= function connectToADSBServer()
+{
+    const net = require('net');
+    const fs = require('fs');
 
-client.connect(31001, '127.0.0.1', function() {
-  console.log('Connected to ADS-B server');
-});
+    var client = new net.Socket();
+    var count=0;
+    var filename='1090Logger_'+(new Date().toISOString().replace(/:/,'_').replace(/:/,'_'))+'.txt';
+    console.log(filename);
 
-client.on('data', function(data) {
-    count=count+1;
-	
-    var datatxt='';
-    for (var i = 0; i < data.length; i++) {
-        datatxt += decimalToHex(data[i])+' ';
-    }
-    
-    console.log('Frame: ' +count+', Length:'+ data.length + ' '+datatxt);
-    fs.appendFileSync(filename,datatxt+'\n');
-});
+    client.connect(31001, '127.0.0.1', function() {
+    console.log('Connected to ADS-B server');
+    });
 
-client.on('close', function() {
-	console.log('Connection to ADS-B closed');
-});
+    client.on('data', function(data) {
+        count=count+1;
+        
+        var datatxt='';
+        for (var i = 0; i < data.length; i++) {
+            datatxt += decimalToHex(data[i])+' ';
+        }
+        
+        console.log('Frame: ' +count+', Length:'+ data.length + ' '+datatxt);
+        fs.appendFileSync(filename,datatxt+'\n');
+    });
 
+    client.on('close', function() {
+        console.log('Connection to ADS-B closed');
+    });
+};
 
 function decimalToHex(d) {
     var hex = Number(d).toString(16).toUpperCase();
