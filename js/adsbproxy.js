6d818d4671388faa65ef20407786823a763f6343
Kanaaan <mapjs@mapjs.com>
2016-02-28 16:01:11 +0100

    * Add the closeAll function in order to clean up opened sockets and server on stop button

--- js/adsbproxy.js
+++ js/adsbproxy.js
@@ -1,45 +1,62 @@
+exports.closeAll= function closeAll()
+{
+    scopes.forEach(function (scope) {
+            // write message to ADSBScope client
+            scope.end();
+        });
+    server.close();
+    client.end();
+}
 
+var server;
+var client;
+// Keep track of the ADSBScope clients
+var scopes = [];
+    
 exports.connectToADSBServer= function connectToADSBServer()
 {
     const net = require('net');
     const fs = require('fs');
 
-    // Keep track of the ADSBScope clients 
-    var scopes = []; 
+    // Keep track of the ADSBScope clients
+    scopes = [];
 
-    var client = new net.Socket();
+    client = new net.Socket();
     var count=0;
     var filename='1090Logger_'+(new Date().toISOString().replace(/:/,'_').replace(/:/,'_'))+'.txt';
     console.log(filename);
-
+    //$("#applicationname").typed({strings: ["1090 Logger"],typeSpeed: 0});
     // Connect to ADSB server
     client.connect(31001, '127.0.0.1', function() {
         console.log('Connected to ADS-B server');
     });
-    
+
     // Create server
-    net.createServer(function (socket) {
+    server=net.createServer(function (socket) {
         // Identify the client
         socket.name = socket.remoteAddress + ":" + socket.remotePort;
         // Put the new client in the list
-        scopes.push(socket); 
+        scopes.push(socket);
     }).listen(31002);
 
     client.on('data', function(data) {
         // dispacth message to clients
-        scopes.forEach(function (scope) { 
-            // write message to ADSBScope client 
+        scopes.forEach(function (scope) {
+            // write message to ADSBScope client
             scope.write(data);
         });
         // Increment messages counter
         count=count+1;
-        
+
         var datatxt='';
         for (var i = 0; i < data.length; i++) {
             datatxt += decimalToHex(data[i])+' ';
         }
         console.log('Frame: ' +count+', Length:'+ data.length + ' '+datatxt);
         fs.appendFileSync(filename,getDateTimeStr()+" "+datatxt+'\n');
+        // Add tp consoletxt
+        var consoletxt='Frame: ' +count+', Length:'+ data.length;
+        $("#consoletxt").html(consoletxt);
     });
 
     client.on('close', function() {
